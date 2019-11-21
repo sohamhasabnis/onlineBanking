6548d3e7367d9873bec9fa6c98d31aa557060d4f
Anindita664 <57948005+Anindita664@users.noreply.github.com>
2019-11-21 12:09:58 +0530

    Add files via upload

---
+++ Banking/src/com/lti/core/entities/Account.java
@@ -0,0 +1,27 @@
+package com.lti.core.entities;
+
+public class Account
+{
+	int account_no;
+	String type;
+	int Balance;
+	public int getAccount_no() {
+		return account_no;
+	}
+	public void setAccount_no(int account_no) {
+		this.account_no = account_no;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public int getBalance() {
+		return Balance;
+	}
+	public void setBalance(int balance) {
+		Balance = balance;
+	}
+	
+}
